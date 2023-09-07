6be14ced478ec715d1eb6d79def40d0e7a274c3b
PhatNMT <nmtphatdev@gmail.com>
2023-09-07 17:10:58 +0700

    Initial commit

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,5 @@
+import 'package:ciname_tickets_booking/utils/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+ThemeData get lightTheme => ThemeData.light().copyWith(
+      extensions: <ThemeExtension<dynamic>>{const AppColors.light()},);
